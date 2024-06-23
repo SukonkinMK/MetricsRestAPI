ee98bd6fbdae30d03c40f0217078d2dbc3457b42
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2024-06-23 19:45:00 +0300

    Созданы контроллеры для получения всех метрик, добавлены классы моделей агентов и хроанилище агентов

---
+++ MetricsManger/Controllers/NetworkMetricsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetworkMetricsController : ControllerBase
+    {
+    }
+}

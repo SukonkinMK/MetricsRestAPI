0e23d02f94dff07235cf549198a550f28a5592fe
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2024-06-23 19:51:34 +0300

    Добавлен проект для unit-тестирования менеджера метрик

---
+++ MetricsManagerTests/CpuMetricsManagerTests.cs
@@ -0,0 +1,37 @@
+﻿using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsManagerTests
+{
+    public class CpuMetricsManagerTests
+    {
+
+
+        private CpuMetricsController _cpuMetricsController;
+
+
+        public CpuMetricsManagerTests()
+        {
+            _cpuMetricsController = new CpuMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnOk()
+        {
+            int agentId = 1;
+            TimeSpan fromTime = TimeSpan.FromSeconds(0);
+            TimeSpan toTime = TimeSpan.FromSeconds(100);
+
+            IActionResult result = _cpuMetricsController.GetMetricsFromAgent(agentId, fromTime, toTime);
+
+            Assert.IsAssignableFrom<IActionResult>(result);
+
+
+        }
+    }
+}
